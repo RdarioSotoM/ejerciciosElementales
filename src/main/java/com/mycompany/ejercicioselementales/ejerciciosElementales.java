bcdcb11a174ae0b92d895f91000cf0fac22a364e
RdariosotoM <rubendario.sotomontana@tiernoparla.es>
2022-09-27 18:54:29 +0200

    añado el SRC

---
+++ src/main/java/com/mycompany/ejercicioselementales/ejerciciosElementales.java
@@ -0,0 +1,7 @@
+package com.mycompany.ejercicioselementales;
+
+public class ejerciciosElementales {
+    public static void main(String[] args) {
+        System.out.println("hola, mundo");
+    }//main
+}//ejerciciosElementales
