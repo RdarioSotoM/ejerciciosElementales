0c9f54adcf91cb73cae19780add6674d40786663
RdariosotoM <rubendario.sotomontana@tiernoparla.es>
2022-09-27 20:08:55 +0200

    Números pares con FOR y WHILE

--- src/main/java/com/mycompany/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/mycompany/ejercicioselementales/ejerciciosElementales.java
@@ -2,6 +2,25 @@ package com.mycompany.ejercicioselementales;
 
 public class ejerciciosElementales {
     public static void main(String[] args) {
-        System.out.println("hola, mundo");
+        
+        //printear nums pares del 1 al 10
+        for (int n = 0; n < 11; n = n + 2) {
+            System.out.println(n);
+        }
+        
+        System.out.println(" ");
+        System.out.println(" ");
+        
+        int m = 0;
+        while (m < 11){
+            System.out.println(m);
+            m = m + 2;
+        }
+        
+       
+        
+        
+        
+        
     }//main
 }//ejerciciosElementales
