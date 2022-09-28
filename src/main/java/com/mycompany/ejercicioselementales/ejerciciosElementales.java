7d581821603c1b6215152b7b7b82eae97bd79b0f
Guillevolcado <blanco_arenzana.guillermo@tiernoparla.es>
2022-09-28 17:51:40 +0200

    bucle con for y los %

--- src/main/java/com/mycompany/ejercicioselementales/ejerciciosElementales.java
+++ src/main/java/com/mycompany/ejercicioselementales/ejerciciosElementales.java
@@ -4,6 +4,7 @@ public class ejerciciosElementales {
     public static void main(String[] args) {
         
         //printear nums pares del 1 al 10
+        /*
         for (int n = 0; n < 11; n = n + 2) {
             System.out.println(n);
         }
@@ -16,8 +17,19 @@ public class ejerciciosElementales {
             System.out.println(m);
             m = m + 2;
         }
+        */
         
-       
+        for(int n = 0; n <= 10; n++){
+        if (n % 2 == 0){
+            System.out.println(n);
+        }}// Pares
+        
+        System.out.println(" ");
+        
+        for(int n = 0; n <= 10; n++){
+        if (n % 2 != 0){
+            System.out.println(n);
+        }}// Impares
         
         
         
